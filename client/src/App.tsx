d45a204ffbc6d6d2e991ff2dfcb6c31f7a8c9b35
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-12-08 23:25:17 +0530

    socket basic connect-disconnect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
+import { io } from "socket.io-client"
+import { useEffect } from "react"
+
 const App = () => {
+
+  const socket = io("http://localhost:8000")
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected", socket.id)
+    })
+
+    // setting up emit and broadcast on console on front-end
+    socket.on("msg", (s) => console.log(s))
+
+    // clean-up function
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
+
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Socket.IO</h1>
     </>
   )
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,34 @@
 import express from "express"
 import { createServer } from "http"
 import { Server } from "socket.io"
+import cors from "cors"
 
 const app = express()
 const httpServer = createServer(app) // providing EXPRESS to HTTP req
 const PORT = 8000
 
-const io = new Server(httpServer) // Initiate connection
+const io = new Server(httpServer, {  // Initiate connection
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+})
+
+app.use(cors())
 
 io.on("connection", (socket) => { // Enabling connection
+    console.log("User connected!")
+    console.log("Id", socket.id)
 
+    // basic connection
+    socket.emit("msg", `Stay connected at ${socket.id}`) // emit sends the message on the particular "id" itself
+    socket.broadcast.emit("msg", `Message sent by ${socket.id}`) // broadcast emit sends the message to all the users except "itself"
+
+    // // disconnection
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id)
+    })
 })
 
 httpServer.listen(PORT, () => {
